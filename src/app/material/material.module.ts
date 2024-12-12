a805c99d5f1adec038e9fc1121e8f034b1d043e6
Abdou1997259 <mohamedabdou1444@gmail.com>
2024-12-12 14:27:51 +0200

    ...

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { NgModel } from "@angular/forms";
+
+@NgModule({
+    imports:[CommonModule],
+
+})
+export class MaterialModule{
+
+}
